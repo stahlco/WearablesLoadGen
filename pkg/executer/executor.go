1a9d6a95fae00424bdd7ad90ad649315ba95372f
stahlco <stahl1consti2@gmail.com>
2025-11-13 18:04:28 +0100

    Impl Executor #1

--- pkg/executer/executor.go
+++ pkg/executer/executor.go
@@ -1,9 +1,30 @@
 package executer
 
-type LoadPattern struct {
-	Distributions map[string]Distribution `yaml:"distributions,omitempty"`
+type Distribution struct {
+	Formula string  `yaml:"formula,omitempty"`
+	Min     float64 `yaml:"min"`
+	Max     float64 `yaml:"max"`
 }
 
-type Distribution struct {
-	Formula string `yaml:"formula"`
+type ExecutorStep struct {
+	Distribution string  `yaml:"distribution,omitempty"`
+	Duration     float64 `yaml:"duration,omitempty"` // in sec
+}
+
+// ExecutionsPattern
+const (
+	Mixed  = "mixed"
+	Linear = "linear"
+)
+
+type ExecutorConfig struct {
+	Name             string         `yaml:"name,omitempty"`
+	ExecutionPattern string         `yaml:"execution-pattern,omitempty"`
+	Duration         float64        `yaml:"duration,omitempty"` // in sec
+	ExecutionSteps   []ExecutorStep `yaml:"steps,omitempty"`
+}
+
+type ExecutionConfig struct {
+	Distributions map[string]Distribution `yaml:"distributions,omitempty"`
+	Executor      ExecutorConfig          `yaml:"executor,omitempty"`
 }
